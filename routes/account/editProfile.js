5218ffa1f4c94ba0af60e514c7cb6a43bb40af90
abdalla-rko <abdallarko@hotmail.com>
2020-06-12 14:49:56 +0200

    edit profile you can know upload images and delete the old one

--- routes/account/editProfile.js
+++ routes/account/editProfile.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const multer = require('multer');
 const path = require('path');
 const crypto = require('crypto');
-const formidable = require('formidable');
+const { Formidable } = require('formidable');
 const mongodb = require('mongodb');
 const assert = require('assert');
 const fs = require('fs');
 const authenticateToken = require('../../config/jwtToken').authenticateToken;
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: '../../.env'});
+}
+
 const router = express.Router();
 
 
@@ -35,12 +38,9 @@ const router = express.Router();
 //   }
 // }).single('profilepic');
 
-
-// ! delete uri
-const mongoURI = 'mongodb+srv://appie:Aborko16sd@oncood-js1zu.mongodb.net/okapi?retryWrites=true&w=majority';
 const connectDatabase = async (req, res) => {
   try {
-    const client = await new mongodb.MongoClient(mongoURI, {
+    const client = await new mongodb.MongoClient(process.env.DB_CONNECTION, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useNewUrlParser: true
@@ -53,43 +53,60 @@ const connectDatabase = async (req, res) => {
       bucketName: 'profile_pics'
     });
 
-    req.files.profilepic.name ? await uploadProfilePic(req, res, bucket) : null;
+    // req.files.profilepic.name ? await uploadProfilePic(req, res, bucket) : null;
     // todo search if file name exist in database else do nothing
-    downloadProfilePic(req, res, bucket)
+    // await await uploadProfilePic(req, res, bucket)
+
+    const checkPictureExist = await db.collection('profile_pics.files').findOne({ filename: "download.jpeg" });
+    if (checkPictureExist) {
+      await downloadProfilePic(req, res, bucket)
+    }
   } catch (error) {
     console.error(error);
   }
 }
 
 const uploadProfilePic = (req, res, bucket) => {
-
-  // todo need to save a unique name with user ID
-  // crypto.randomBytes(16, (err, buf) => {
-  //   if (err) {
-  //     return reject(err);
-  //   }
-  //   const filename = buf.toString('hex') + path.extname(file.originalname);
-  //   console.log(filename);
-    
-  // });
-  // Use bucket...
-  fs.createReadStream(req.files.profilepic.path).
-  pipe(bucket.openUploadStream(req.files.profilepic.name)).
-  on('error', function(error) {
-    assert.ifError(error);
-  }).
-  on('finish', function() {
-    console.log('done!');
-    // process.exit(0);
-  });
+  if (req.files.profilepic) {
+    if (req.files.profilepic.type === 'image/jpeg' || req.files.profilepic.type === 'image/png' || req.files.profilepic.type === 'image/jpg') {
+      console.log('path: ' + req.files.profilepic.name)
+      console.log('ext: ' + req.files.profilepic.type)
+      fs.createReadStream(req.files.profilepic.path).
+      pipe(bucket.openUploadStream("profile" + path.extname(req.files.profilepic.name))).
+      on('error', function(error) {
+        assert.ifError(error);
+      }).
+      on('finish', function() {
+        console.log('done!');
+        return;
+        // process.exit(0);
+        });
+    }
+  }
+  console.log("not image or image is bigger than 10mb");
+  
+  return req.flash('profile', 'Not an image') // todo in fron-end
 }
 
 const downloadProfilePic = (req, res, bucket) => {
   // find user profile
-  bucket.findOne({})
+  // bucket.findOne({})
   // Use bucket...
-  bucket.openDownloadStreamByName("download.jpeg").
-  pipe(fs.createWriteStream('profile.jpeg')).
+
+
+  const pathToOldProfilePic = 'public/uploads/profile/profile.jpeg';
+  fs.unlink(pathToOldProfilePic, function(err) {
+    if (err) {
+      // console.error(err)
+    } else {
+      console.log("successfully deleted the file.");
+      
+    }
+  })
+
+
+  bucket.openDownloadStreamByName("haha.jpeg").
+  pipe(fs.createWriteStream(`public/uploads/profile/profile.jpeg`)).
   on('error', function(error) {
     assert.ifError(error);
   }).
@@ -129,7 +146,6 @@ router.get('/upload', authenticateToken, (req, res) => {
 })
 
 router.post('/upload', authenticateToken, formparser, (req, res) => {
-  
   // res.render('account/editProfile.ejs', {
   //   msg: 'File Uploaded!',
   //   file: `uploads/${req.file.filename}`,
@@ -141,7 +157,7 @@ router.post('/upload', authenticateToken, formparser, (req, res) => {
 })
 
 function formparser(req, res , next) {
-  const form = new formidable.IncomingForm();
+  const form = new Formidable({ maxFileSize: 10 * 1024 * 1024 });
   form.parse(req, function (err, fields, files) {
     req.files = files;
     next();
